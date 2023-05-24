5cfc77c0039f05f284deb046f0e4b20e90ef1074
David Lowry <DavidLowry64@gmail.com>
2023-05-23 21:13:52 -0400

    initial commit

---
+++ src/main/java/com/ttrc/stockservice/StockServiceApplication.java
@@ -0,0 +1,14 @@
+package com.ttrc.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+		System.out.println("Hello Worold");git a
+	}
+
+}
