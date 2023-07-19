915624e95e426fb11098988b85ec18843620aec3
David Lowry <DavidLowry64@gmail.com>
2023-07-19 17:53:43 -0400

    Adding Service Discovery

--- src/main/java/com/ttrc/stockservice/StockServiceApplication.java
+++ src/main/java/com/ttrc/stockservice/StockServiceApplication.java
@@ -2,13 +2,17 @@ package com.ttrc.stockservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class StockServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockServiceApplication.class, args);
-		System.out.println("Hello Worold");
+		System.out.println("Hello World Sotck srieive");
 	}
 
 }
